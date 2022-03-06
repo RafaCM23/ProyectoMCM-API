59961ea0873f0ed0abaa32cbe352e2651de26e51
RafaelCuberos <surfearr2@gmail.com>
2022-03-06 20:08:37 +0100

    probando cambios para heroku

--- src/main/java/com/example/demo/security/JWTUtil.java
+++ src/main/java/com/example/demo/security/JWTUtil.java
@@ -3,8 +3,6 @@ package com.example.demo.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTCreationException;
-import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -17,7 +15,7 @@ public class JWTUtil {
     @Value("${jwt_secret}")
     private String secret;
 
-    public String generateToken(String email) throws IllegalArgumentException, JWTCreationException {
+    public String generateToken(String email) throws Exception {
         return JWT.create()
                 .withSubject("User Details")
                 .withClaim("email", email)
@@ -26,7 +24,7 @@ public class JWTUtil {
                 .sign(Algorithm.HMAC256(secret));
     }
 
-    public String validateTokenAndRetrieveSubject(String token)throws JWTVerificationException {
+    public String validateTokenAndRetrieveSubject(String token)throws Exception {
         JWTVerifier verifier = JWT.require(Algorithm.HMAC256(secret))
                 .withSubject("User Details")
                 .withIssuer("YOUR APPLICATION/PROJECT/COMPANY NAME")
